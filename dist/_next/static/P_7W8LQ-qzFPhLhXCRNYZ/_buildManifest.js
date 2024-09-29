626651295fdd5b6da06a50f4b1a3bea345c998f1
lhjok <lhjok@live.cn>
2024-09-30 04:34:17 +0800

    first commit

---
+++ dist/_next/static/P_7W8LQ-qzFPhLhXCRNYZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/951-9206c132af65123a.js","static/chunks/pages/index-c91f81b9d9792f0a.js"],"/_error":["static/chunks/pages/_error-8447282b6bcee29e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
